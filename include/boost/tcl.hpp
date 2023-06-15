717040cd7151f9c570cb4190dbfff54fd76093b9
Klemens Morgenstern <klemens.d.morgenstern@gmail.com>
2023-06-16 00:03:38 +0800

    adjusted tcl.hpp

--- include/boost/tcl.hpp
+++ include/boost/tcl.hpp
@@ -9,13 +9,22 @@
 #define BOOST_TCL_H
 
 #include <boost/tcl/allocator.hpp>
-#include <boost/tcl/cast.hpp>
-#include <boost/tcl/command.hpp>
-#include <boost/tcl/exception.hpp>
+#include <boost/tcl/async.hpp>
 #include <boost/tcl/builtin.hpp>
+#include <boost/tcl/cast.hpp>
+#include <boost/tcl/channel.hpp>
+#include <boost/tcl/class.hpp>
+#include <boost/tcl/command.hpp>
+#include <boost/tcl/enum.hpp>
+#include <boost/tcl/eval.hpp>
+#include <boost/tcl/event.hpp>
+#include <boost/tcl/exception.hpp>
+#include <boost/tcl/expr.hpp>
 #include <boost/tcl/interpreter.hpp>
 #include <boost/tcl/object.hpp>
 #include <boost/tcl/package.hpp>
 #include <boost/tcl/string_command.hpp>
+#include <boost/tcl/thread.hpp>
+#include <boost/tcl/var.hpp>
 
 #endif //BOOST_TCL_H
