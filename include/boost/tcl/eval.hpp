e3c40ce4ded2c177abfbcf30f23ba48343b5d4b2
Klemens Morgenstern <klemens.d.morgenstern@gmail.com>
2023-06-15 23:59:55 +0800

    switched eval to result & added token span overload

--- include/boost/tcl/eval.hpp
+++ include/boost/tcl/eval.hpp
@@ -14,98 +14,119 @@
 #include <boost/tcl/object.hpp>
 
 #include <boost/core/detail/string_view.hpp>
+#include <boost/core/span.hpp>
 
 namespace boost::tcl
 {
 
 template<typename T = object_ptr>
-T eval(Tcl_Interp * interp,
-       core::string_view script,
-       int flags = 0)
+result<T> eval(Tcl_Interp * interp,
+               core::string_view script,
+               int flags = 0)
 {
   auto res = Tcl_EvalEx(interp, script.data(), script.size(), flags);
   if (res != TCL_OK)
-    throw_result(interp);
-  return cast<T>(interp, Tcl_GetObjResult(interp));
+    return result<T>{system::in_place_error, Tcl_GetObjResult(interp)};;
+  return result<T>{system::in_place_value, cast<T>(interp, Tcl_GetObjResult(interp))};
 }
 
 template<typename T = object_ptr>
-T eval(const interpreter_ptr & interp,
-       core::string_view script,
-       int flags = 0)
+result<T> eval(const interpreter_ptr & interp,
+               core::string_view script,
+               int flags = 0)
 {
   return eval<T>(interp.get(), script, flags);
 }
 
+
 template<typename T = object_ptr>
-T eval_file(Tcl_Interp * interp,
-            const std::string & file,
-            int flags = 0)
+result<T> eval(Tcl_Interp * interp,
+                span<Tcl_Token> tokens)
+{
+  auto res = Tcl_EvalTokensStandard(interp, tokens.data(), tokens.size());
+  if (res != TCL_OK)
+    return result<T>{system::in_place_error, Tcl_GetObjResult(interp)};;
+  return result<T>{system::in_place_value, cast<T>(interp, Tcl_GetObjResult(interp))};
+}
+
+template<typename T = object_ptr>
+result<T> eval(const interpreter_ptr & interp,
+               span<Tcl_Token> tokens)
+{
+  return eval<T>(interp.get(), tokens);
+}
+
+
+
+template<typename T = object_ptr>
+result<T> eval_file(Tcl_Interp * interp,
+                    const std::string & file,
+                    int flags = 0)
 {
   auto res = Tcl_EvalFile(interp, file.c_str());
   if (res != TCL_OK)
-    throw_result(interp);
-  return cast<T>(interp, Tcl_GetObjResult(interp));
+    return result<T>{system::in_place_error, Tcl_GetObjResult(interp)};;
+  return result<T>{system::in_place_value, cast<T>(interp, Tcl_GetObjResult(interp))};
 }
 
 template<typename T = object_ptr>
-T eval_file(const interpreter_ptr & interp,
-       const std::string & file)
+result<T> eval_file(const interpreter_ptr & interp,
+                    const std::string & file)
 {
   return eval_file<T>(interp.get(), file);
 }
 
 
 template<typename T = object_ptr>
-T eval_file(Tcl_Interp * interp,
-            const char * file,
-            int flags = 0)
+result<T> eval_file(Tcl_Interp * interp,
+                    const char * file,
+                    int flags = 0)
 {
   auto res = Tcl_EvalFile(interp, file);
   if (res != TCL_OK)
-    throw_result(interp);
-  return cast<T>(interp, Tcl_GetObjResult(interp));
+    return result<T>{system::in_place_error, Tcl_GetObjResult(interp)};;
+  return result<T>{system::in_place_value, cast<T>(interp, Tcl_GetObjResult(interp))};
 }
 
 template<typename T = object_ptr>
-T eval_file(const interpreter_ptr & interp,
-            const char * file)
+result<T> eval_file(const interpreter_ptr & interp,
+                    const char * file)
 {
   return eval_file<T>(interp.get(), file);
 }
 
 
 template<typename T = object_ptr>
-T global_eval(Tcl_Interp * interp,
-       const char * script)
+result<T> global_eval(Tcl_Interp * interp,
+                      const char * script)
 {
   auto res = Tcl_GlobalEval(interp, script);
   if (res != TCL_OK)
-    throw_result(interp);
-  return cast<T>(interp, Tcl_GetObjResult(interp));
+    return result<T>{system::in_place_error, Tcl_GetObjResult(interp)};;
+  return result<T>{system::in_place_value, cast<T>(interp, Tcl_GetObjResult(interp))};
 }
 
 template<typename T = object_ptr>
-T global_eval(const interpreter_ptr & interp,
-       core::string_view script)
+result<T> global_eval(const interpreter_ptr & interp,
+                      core::string_view script)
 {
   return global_eval<T>(interp.get(), script);
 }
 
 
 template<typename T = object_ptr>
-T global_eval(Tcl_Interp * interp,
-              const std::string & script)
+result<T> global_eval(Tcl_Interp * interp,
+                      const std::string & script)
 {
   auto res = Tcl_GlobalEval(interp, script.c_str());
   if (res != TCL_OK)
-    throw_result(interp);
-  return cast<T>(interp, Tcl_GetObjResult(interp));
+    return result<T>{system::in_place_error, Tcl_GetObjResult(interp)};;
+  return result<T>{system::in_place_value, cast<T>(interp, Tcl_GetObjResult(interp))};
 }
 
 template<typename T = object_ptr>
-T global_eval(const interpreter_ptr & interp,
-              const std::string & script)
+result<T> global_eval(const interpreter_ptr & interp,
+                      const std::string & script)
 {
   return global_eval<T>(interp.get(), script);
 }
