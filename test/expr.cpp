e098a63a855a14a5e028266e141d6c0e8ed16168
Klemens Morgenstern <klemens.d.morgenstern@gmail.com>
2023-06-16 00:01:21 +0800

    added expr functions.

---
+++ test/expr.cpp
@@ -0,0 +1,23 @@
+//
+// Copyright (c) 2023 Klemens Morgenstern (klemens.morgenstern@gmx.net)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#include <boost/tcl/expr.hpp>
+#include <boost/tcl/builtin/integral.hpp>
+#include <boost/tcl/builtin/string.hpp>
+
+#include "doctest.h"
+
+using namespace boost;
+
+extern Tcl_Interp *interp;
+
+
+TEST_CASE("expr")
+{
+  CHECK(tcl::expr<int>(interp, 42) == 42);
+  CHECK(tcl::expr<int>(interp, "23 + 12") == 35);
+  CHECK_THROWS(tcl::expr<int>(interp, "12 / 0").value());
+}
